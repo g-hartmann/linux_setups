acb72f33b93a90be2d6cb7456918f9f00d2b0172
ghartmann <45218534+g-hartmann@users.noreply.github.com>
2020-07-16 20:03:09 +0200

    Added untested pacman setup for Arch/Manjaro

---
+++ setup_pacman.sh
@@ -0,0 +1,28 @@
+sudo pacman -Syu
+sudo pacman -S vim
+sudo pacman -S git
+sudo pacman -S cmake
+
+ssh-keygen -b 4096
+cat ~/.ssh/id_rsa.pub
+echo ''
+echo '*** Copy key with CTRL-SHIFT-C and add to github ***'
+
+cd ~
+git init
+echo '*** adding hartmann.gregor@outlook.com to git config ***'
+git config --global user.email "hartmann.gregor@outlook.com"
+git config --global user.name "Gregor Hartmann"
+git fetch
+git reset origin/master
+git submodule update --init --recursive
+git restore ~/.bashrc.gitmodules
+git restore ~/.vim/vimrc
+git restore ~/.bashrc
+git pull
+
+cd ~/.vim/bundle/YouCompleteMe/
+python3 install.py
+vim +PluginInstall +qall
+
+source ~/.bashrc
