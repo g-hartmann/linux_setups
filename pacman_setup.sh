19ea448999a4f442e5c043dcecd1413520f9d589
ghartmann <45218534+g-hartmann@users.noreply.github.com>
2020-07-20 18:02:53 +0200

    Updated pacman_setup.sh

---
+++ pacman_setup.sh
@@ -0,0 +1,32 @@
+sudo pacman -Syu
+sudo pacman -S vim
+sudo pacman -S git
+sudo pacman -S cmake
+sudo pacman -S openssh
+
+if [ ! -f $HOME/.ssh/id_rsa ]; then
+	ssh-keygen -b 4096
+fi
+cat ~/.ssh/id_rsa.pub
+echo ''
+echo '*** Copy key with CTRL-SHIFT-C and add to github ***'
+
+cd ~
+echo '*** adding hartmann.gregor@outlook.com to git config ***'
+git init
+git config --global user.email "hartmann.gregor@outlook.com"
+git config --global user.name "Gregor Hartmann"
+git remote add origin https://github.com/g-hartmann/dotfiles.git
+git fetch
+git reset origin/master
+git submodule update --init --recursive
+# git restore ~/.gitmodules
+git restore ~/.vim/vimrc
+git restore ~/.bashrc
+git pull
+
+cd ~/.vim/bundle/YouCompleteMe/
+python3 install.py
+vim +PluginInstall +qall
+
+source ~/.bashrc
